af8a2836dffed74a37d9080a26a5c23985bea231
agar91 <agnieszkarapa@gmail.com>
2021-03-16 13:27:15 +0100

    view registration added

--- bike-ui/src/app/app-routing.module.ts
+++ bike-ui/src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './components/admin/admin.component';
 import { HomeComponent } from './components/home/home.component';
+import { ViewRegistrationComponent } from './components/view-registration/view-registration.component';
 
 const routes: Routes = [
-{
-	path: '',
-	component: HomeComponent
-},
-{
-	path: 'admin',
-	component: AdminComponent
-}
+	{
+		path: '',
+		component: HomeComponent
+	},
+	{
+		path: 'view-registration',
+		component: ViewRegistrationComponent
+	},
+	{
+		path: 'admin',
+		component: AdminComponent
+	}
 
 ];
 
